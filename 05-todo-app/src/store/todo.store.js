1d0611631772e58ce131f6677af23b0dcef2e1bc
namascasia <namascasia@gmail.com>
2023-09-22 11:38:39 -0700

    todo.store construccion de metodos sin implementacion

--- 05-todo-app/src/store/todo.store.js
+++ 05-todo-app/src/store/todo.store.js
@@ -6,6 +6,7 @@ const Filters = {
     Completed: 'Completed',
     Pending: 'Pending'
 }
+
 const state = {
     todos: [
         new Todo('Hola Aleman'),
@@ -20,6 +21,55 @@ const initStore = () => {
     console.log('InitStore :)'); 
 }
 
+
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * Agrega un todo
+ * @param {String} description 
+ */
+const addTodo = ( description ) => {
+    throw new Error('Not implemented');
+}
+/**
+ * Marca estado de terminado o pendiente
+ * @param {String} todoId identified
+ */
+const toggleTodo = ( todoId ) => {
+    throw new Error('Not implemented');
+}
+/**
+ * elimina un todo
+ * @param {String} todoId identified 
+ */
+const deleteTodo = ( todoId ) => {
+    throw new Error('Not implemented');
+}
+
+const deleteCompleted = () => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * Cambia el estado del filter
+ * @param {String} newFilter  
+ */
+const setFilter = ( newFilter = Filters.All ) => {
+    throw new Error('Not implemented');
+}
+
+const getCurrentFilter = () => {
+    throw new Error('Not implemented');
+} 
+
 export default {
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
