0c2a4fe4338ebd6b0ba9acee559a1c63807ef589
namascasia <namascasia@gmail.com>
2023-09-26 11:49:08 -0700

    renderización parcialmente completo

--- 05-todo-app/src/todos/app.js
+++ 05-todo-app/src/todos/app.js
@@ -1,13 +1,28 @@
 import html from './app.html?raw'; //raw pasar en crudo la importanción 
+import todoStore from '../store/todo.store';
+import { renderTodos } from './use-cases';
+
+const ElementIDs = {
+    TodoList: '.todo-list',
+}
+
 /**
  * Redenrizar la aplicacion
  * @param {String} elementId 
  */
 export const App = (elementId) => {
+
+    //Llamada cada vez que quiera re dibujar los Todos
+    const displayTodos = () => {
+        const todos = todoStore.getTodos( todoStore.getCurrentFilter() );
+        renderTodos(ElementIDs.TodoList, todos);
+    }
+
     //Cuando la App() se llama
     (()=>{
         const app = document.createElement('div');
         app.innerHTML = html; //{{ name }} otra manera de renderizar
         document.querySelector(elementId).append( app );
+        displayTodos();
     })();
 }
---
+++ 05-todo-app/src/todos/use-cases/render-todos.js
@@ -0,0 +1,22 @@
+import { Todo } from "../models/todo.model";
+import { createTodoHTML } from "./";
+
+let element;
+
+/**
+ * renderiza los todos 
+ * @param {String} elementId 
+ * @param {Todo} todos 
+ */
+export const renderTodos = ( elementId, todos = [] ) => {
+    if( !element )
+        element = document.querySelector(elementId);
+    
+    if( !element ) throw new Error(`Element ${ elementId } not found`);
+
+    element.innerHTML = '';
+
+    todos.forEach( todo => {
+        element.append( createTodoHTML(todo) )
+    });
+}
